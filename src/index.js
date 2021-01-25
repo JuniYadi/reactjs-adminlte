5777084806bf2c7f708b3577941e3e3110a404e8
JuniYadi <juniyadi94@gmail.com>
2021-01-25 16:34:12 +0700

    Include AdminLTE Dependency to Index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import adminlte dependency css
+import 'admin-lte/dist/css/adminlte.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+// import adminlte dependency js
+import 'admin-lte/dist/js/adminlte';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
