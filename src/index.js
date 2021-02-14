66c056fb4093eeab5adac7a6681d2fc9be8e274f
JuniYadi <juniyadi94@gmail.com>
2021-02-14 23:34:36 +0700

    Added Redux and Configuration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider  } from 'react-redux';
 
 import './includes/adminlte';
 import App from './App';
+import configureStore from "./redux/configureStore";
 import reportWebVitals from './reportWebVitals';
 
-// import redux
-// import { Provider } from 'react-redux';
+const store = configureStore();
+
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/actions/AuthAction.js
@@ -0,0 +1,23 @@
+import * as constants from '../constants';
+
+export const loginUser = (data, onSuccess, onError) => ({
+    type: constants.API,
+    payload: {
+        method: 'POST',
+        url: '/auth/login',
+        data,
+        success: (response) => (setUserInfo(response)),
+        postProcessSuccess: onSuccess,
+        postProcessError: onError,
+    }
+})
+
+const setUserInfo = (data) => {
+    // const parseToken = JSON.parse(data);
+    const userInfo = {
+        token: data.token,
+        isLoggedIn: true,
+    }
+    localStorage.setItem('USER_INFO', JSON.stringify(userInfo));
+    return { type: constants.SET_USER_INFO, payload: userInfo }
+}
