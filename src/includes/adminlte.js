e6c672c8b98a67cf1613a0405f6e6fe53e310fc1
JuniYadi <juniyadi94@gmail.com>
2021-02-14 22:49:21 +0700

    Update Overlay and Dayjs

--- src/includes/adminlte.js
+++ src/includes/adminlte.js
@@ -3,3 +3,5 @@ import './popper';
 import 'admin-lte';
 import 'admin-lte/dist/css/adminlte.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import 'overlayscrollbars';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
